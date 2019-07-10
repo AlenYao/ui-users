34c0d4176fe53404601a443985b497918bb163d7
Yevhenii Maltsev <yevhenii_maltsev@epam.com>
2019-07-10 18:19:30 +0300

    UIU-1070: Update tests to cover changes in due date modal in stripes-smart-components

--- test/bigtest/interactors/open-loans.js
+++ test/bigtest/interactors/open-loans.js
@@ -4,7 +4,17 @@ import {
   collection,
   count,
   Interactor,
+  property,
 } from '@bigtest/interactor';
+
+import ButtonInteractor from '@folio/stripes-components/lib/Button/tests/interactor'; // eslint-disable-line
+
+const getFormattedDate = () => {
+  const dueDate = new Date();
+  const day = ('0' + dueDate.getDate()).slice(-2);
+  const month = ('0' + (dueDate.getMonth() + 1)).slice(-2);
+  return month + '/' + day + '/' + dueDate.getFullYear();
+};
 
 @interactor class BulkOverrideModal {
   static defaultScope = '#bulk-override-modal';
@@ -18,6 +28,17 @@ import {
   overrideButton = scoped('[data-test-override-button]')
 }
 
+@interactor class ChangeDueDateOverlay {
+  static defaultScope = '[data-test-change-due-date-dialog]';
+
+  successDueDateChangeAlerts = collection('[data-test-success-due-date-change-alert]');
+  dueDateCalendarButton = new ButtonInteractor('[data-test-calendar-button]');
+  saveButton = new ButtonInteractor('[data-test-change-due-date-save-button]');
+  cancelButton = new ButtonInteractor('[data-test-change-due-date-cancel-button]');
+  requestsCount = scoped('[data-test-requests-count]');
+  isSaveButtonDisabled = property('[data-test-change-due-date-save-button]', 'disabled');
+}
+
 @interactor class OpenLoans {
   static defaultScope = '[data-test-open-loans]';
 
@@ -26,10 +47,13 @@ import {
   actionDropdowns = collection('[data-test-actions-dropdown]');
   actionDropdownRequestQueue = new Interactor('[data-test-dropdown-content-request-queue]');
   actionDropdownRenewButton = new Interactor('[data-test-dropdown-content-renew-button]');
+  actionDropdownChangeDueDateButton = new Interactor('[data-test-dropdown-content-change-due-date-button]');
   requestsCount = count('[data-test-list-requests]');
 
   bulkRenewalModal = new BulkRenewalModal();
   bulkOverrideModal = new BulkOverrideModal();
+  changeDueDateOverlay = new ChangeDueDateOverlay();
+  dueDateCalendarCellButton = new ButtonInteractor(`[data-test-date="${getFormattedDate()}"]`);
 
   whenLoaded() {
     return this.when(() => this.list.isVisible);
